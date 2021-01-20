8136e873b271d329056cecd89e8c29bb92f145c6
Leonardo Capogna <leonardocapogna@gmail.com>
2021-01-20 15:34:52 +0100

    start

---
+++ index.php
@@ -0,0 +1,44 @@
+<!-- <?php
+  // commento PHP
+  $nome = $_GET["nome"];
+  $cognome = $_GET["cognome"];
+
+  $nome = "leonardo";
+  $cognome = "capogna";
+
+  $badWord = "dolor";
+
+  echo str_replace($badWord, "***", $text);
+
+  $text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  $arrayText = explode(",", $text);
+ ?>
+  -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>snack-1</title>
+  </head>
+  <body>
+
+
+
+
+  </body>
+
+</html>
+
+<!-- ESEMPI -->
+<!-- <h1>Nome completo: <?php echo $nome . " " . $cognome; ?></h1>
+<h2>Nome: <?php echo $nome; ?></h2>
+<h2>Cognome: <?php echo $cognome; ?></h2>
+<p>Variabile GET: <?php echo $_GET["nome"]; ?></p>
+<p><?php var_dump($arrayText); ?></p>
+<h4><?php echo trim($arrayText[1]); ?></h4>
+<?php echo str_replace("dolor", "***", $text); ?>
+<p>Lunghezza stringa: <?php echo strlen($text); ?></p>
+<h5><?php echo ucwords($text); ?></h5> -->
+<!-- /ESEMPI -->
